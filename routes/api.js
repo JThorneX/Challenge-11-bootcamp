9c9471e7a441f28ea151aeba21606c765be782ef
K'G <idragons1720@gmail.com>
2023-03-16 20:17:25 -0600

    api routes done(?)

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+//rename to api instead of index.js for the requirement
+//const express = require("express");
+const fs = require("fs");
+
+//const app = express();
+//app.use("/notes", notesRouter);
+
+//parse and read json from db
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      let dbJSON = JSON.parse(data);
+      res.send(dbJSON);
+    });
+  });
+
+  //post - receive new note to save to request body -> add to db.json -> return to client (unique id)
+  app.post("/api/notes", function (req, res) {
+    const clientNote = req.body;
+
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      dbJSON = JSON.parse(data);
+      dbJSON.push(clientNote);
+      //says use package but ++ works?
+      let noteIndex = 0;
+
+      dbJSON.forEach((clientNote, index) => {
+        clientNote.id = noteIndex;
+        noteIndex++;
+        return dbJSON;
+      });
+      noteString = JSON.stringify(dbJSON);
+
+      fs.writeFile("./db/db.json", noteString, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send("Note Saved.");
+  });
+};
