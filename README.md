d62d78371264dde943f2a848e0346ba946fa7cc6
K'G <idragons1720@gmail.com>
2023-03-16 19:33:36 -0600

    html routes finished(?)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Challenge-11-bootcamp
-Week 7 DU bootcamp challenge
+# Challenge 11 Note Taker
+### Week 6 DU bootcamp challenge
+
+### License
+MIT
+
+### Description
+Fixing starter code to link the two pages and enable the save button to create notes.
+
+### Usage
+
+
+### Links
+Heroku
---
+++ routes/server.js
@@ -0,0 +1,17 @@
+//html routes
+//const express = require("express");
+//const fs = require("fs");
+const path = require("path");
+
+module.exports = function (app) {
+  // GET requests when users visit a different page
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  );
+  app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+  );
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  );
+};
